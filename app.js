3e0b52f32d6968d29d88cbf760a5e48d81930405
Jesust9140 <jesust9140@gmail.com>
2025-06-06 12:40:36 -0700

    everything done but last question

--- app.js
+++ app.js
@@ -1,187 +1,125 @@
-const flavors = ['vanilla', 'chocolate', 'strawberry'];
-
-const iceCreamFlavors = flavors.map((flavor) => {
-  return `${flavor} ice cream`;
-});
-
-console.log(iceCreamFlavors); 
-// Prints: ['vanilla ice cream', 'chocolate ice cream', 'strawberry ice cream']
-
 // ! Exercise 1:
 // a. Use the `map()` method to iterate over the provided `nums` array
-//
-// b. Use the callback function of the `map()` method you wrote to
-//    create a new array where each value is multiplied by 2.
-//    Name the new array `numsTimesTwo`.
-//
+// b. Create a new array where each value is multiplied by 2, named `numsTimesTwo`
 // c. Console log the new array.
-//
-// Starting code (don't modify this):
 
 const nums = [13, 87, 2, 89, 12, 4, 90, 63];
-
-// Your code here:
-
-const petsArray = ['Rover', 'Snuffles', 'Toto', 'Spot'];
-
-const [firstPet, secondPet] = petsArray;
-
-console.log(firstPet);
-// Prints: 'Rover'
-console.log(secondPet);
-// Prints: 'Snuffles'
-
-// Equivalent in traditional bracket notation:
-console.log(petsArray[0]);
-// Prints: 'Rover'
-console.log(petsArray[1]);
-// Prints: 'Snuffles'
+const numsTimesTwo = nums.map(num => num * 2);
+console.log("Exercise 1 - numsTimesTwo:", numsTimesTwo);
+// Output: [26, 174, 4, 178, 24, 8, 180, 126]
 
 
 // ! Exercise 2:
-// a. Given the provided `pizzaToppings` array, use destructuring to extract 
-//    the first and second values and place them into variables. Name the 
-//    variable that corresponds to the first value `firstIngredient`. Name the 
-//    variable that corresponds to the second value `secondIngredient`.
-//
-// b. Console log the `firstIngredient` and `secondIngredient` variables.
-//
-// Starting code (don't modify this):
+// a. Destructure the first and second values of `pizzaToppings`
+// b. Name them `firstIngredient` and `secondIngredient`
+// c. Console log both
 
 const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+const [firstIngredient, secondIngredient] = pizzaToppings;
 
-// Your code here:
-
-const person = {
-  name: 'Alex',
-  role: 'Software Engineer',
-};
-
-// Object destructuring:
-const { name, role } = person;
-
-console.log(name);
-// Prints: 'Alex'
-console.log(role);
-// Prints: 'Software Engineer'
-
-// Equivalent in traditional dot notation:
-console.log(person.name); 
-// Prints: 'Alex'
-console.log(person.role); 
-// Prints: 'Software Engineer'
+console.log("Exercise 2 - firstIngredient:", firstIngredient);   // Pineapple
+console.log("Exercise 2 - secondIngredient:", secondIngredient); // Olives
 
 
 // ! Exercise 3:
-// a. Given the provided `car` object, use destructuring to create two
-//    variables: `make` and `model` that will hold the respective values.
-//
-// b. Console log the `make` and `model` variables.
-//
-// Starting code (don't modify this):
+// a. Destructure `make` and `model` from the car object
+// b. Console log both
 
 const car = {
   make: 'Audi',
   model: 'Q5',
 };
 
-// Your code here:
+const { make, model } = car;
 
-// ! Exercise 4: 
-// a. Duplicate the provided `morePizzaToppings` array using the spread 
-//    operator and assign it to a variable named `uncontroversialPizzaToppings`.
-// 
-// b. Console log the `uncontroversialPizzaToppings` variable.
-//
-// Starting code (don't modify this):
+console.log("Exercise 3 - make:", make);   // Audi
+console.log("Exercise 3 - model:", model); // Q5
+
+// ! Exercise 4:
+// a. Duplicate the array `morePizzaToppings` using the spread operator
+// b. Store it in `uncontroversialPizzaToppings` and console log it
 
 const morePizzaToppings = ['Cheese', 'Sauce'];
+const uncontroversialPizzaToppings = [...morePizzaToppings];
 
-// Your code here:
+console.log("Exercise 4 - uncontroversialPizzaToppings:", uncontroversialPizzaToppings);
+// Output: ['Cheese', 'Sauce']
 
 // ! Exercise 5:
-// a. Duplicate the provided `anotherCar` object and spread its values into a 
-//    new variable named `myCar`.
-//
-// b. Change the `make` and `model` properties of the `myCar` object to new 
-//    values.
-//
-// c. Console log both objects and observe the results.
-//
-// Starting code (don't modify this):
+// a. Clone `anotherCar` into `myCar` using the spread operator
+// b. Change `make` and `model` in `myCar`
+// c. Log both to confirm changes
 
 const anotherCar = {
   make: 'Toyota',
   model: 'RAV4',
 };
 
-// Your code here:
+const myCar = { ...anotherCar };
+myCar.make = 'Honda';
+myCar.model = 'Civic';
+
+console.log("Exercise 5 - anotherCar:", anotherCar); // { make: 'Toyota', model: 'RAV4' }
+console.log("Exercise 5 - myCar:", myCar);           // { make: 'Honda', model: 'Civic' }
 
 
+// ! Exercise 6:
+// a. Define a variable `propertyName`
+// b. Create an object `userProfile` using `propertyName` as key
+// c. Console log the object
+
+const propertyName = 'username';
+const userProfile = {
+  [propertyName]: 'Jesust9140'
+};
+
+console.log("Exercise 6 - userProfile:", userProfile);
+// Output: { username: 'Jesust9140' }
+
+
+export default 'Matt';
+export const computer = 'MacBook Pro';
+
+const age = 43;
+const job = 'programmer';
+export { age, job };
 
 
 // ! Exercise 8:
-// a. Create a function with two parameters, `noun` and `adjective`.
-// 
-// b. Give `noun` a default value of "cat" and `adjective` a default value of 
-//    "orange".
-//
-// c. The function should log a sentence 'The cat is orange.' by default, but 
-//    should substitute the appropriate parameters when it is supplied with 
-//    arguments.
-//
-// Your code here:
+// a. Create a function with default parameters `noun = 'cat'` and `adjective = 'orange'`
+// b. Log the sentence using a template string
 
-// ! Exercise 9:
-// a. Convert the following `if...else` statement into a ternary:
-//
-//    if (pizza === 'tasty') {
-//      console.log('yum');
-//    } else {
-//      console.log('yuck');
-//    }
-//
-// Starting code (don't modify this):
+function describeAnimal(noun = "cat", adjective = "orange") {
+  console.log(`Exercise 8 - The ${noun} is ${adjective}.`);
+}
 
-const pizza = 'tasty';
-
-// Your code here:
+describeAnimal();               // The cat is orange.
+describeAnimal('dog', 'brown'); // The dog is brown.
 
 
 // ! Exercise 9:
-// a. Convert the following `if...else` statement into a ternary:
-//
-//    if (pizza === 'tasty') {
-//      console.log('yum');
-//    } else {
-//      console.log('yuck');
-//    }
-//
-// Starting code (don't modify this):
+// Convert this:
+// if (pizza === 'tasty') {
+//   console.log('yum');
+// } else {
+//   console.log('yuck');
+// }
 
 const pizza = 'tasty';
+console.log("Exercise 9 -", pizza === 'tasty' ? 'yum' : 'yuck');
 
-// Your code here:
 
-// ! Exercise 10:
-// ! 10.1: Set language
-// a. Construct a single line of code that assigns a default value using the 
-//    logical OR operator. This line should match the logic of the following 
-//    statement:
-//
-//    "lang is equal to localLangConfig or the default value of English."
-//
-// b. Create a variable called `lang`.
-//
-// c. Assign `lang` the value of localLangConfig or 'en' as a default if 
-//    `localLangConfig is falsy.
-//
-// d. Log the value of `lang` to the console.
-//
-// Your code here (localLangConfig is provided to get you started):
+// ! 10.1
+const localLangConfig = null;
+const lang = localLangConfig || 'en';
 
-// Simulated language configuration (change this variable to test)
-const localLangConfig = null; // Change to 'es', 'fr', etc., or leave it `null`.
+console.log("Exercise 10.1 - lang:", lang); // en
+
+// ! 10.2
+const savedUserTheme = null;
+const theme = savedUserTheme || 'light';
+
+console.log("Exercise 10.2 - theme:", theme); // light
 
 
 // ! Exercise 11:
---
+++ importingFile.js
@@ -0,0 +1,4 @@
+import name, { computer, age, job } from './exportingFile.js';
+
+console.log("Exercise 7 - Imports:", name, computer, age, job);
+// Output: Matt MacBook Pro 43 programmer
